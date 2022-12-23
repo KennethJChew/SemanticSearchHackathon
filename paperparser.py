494c505e7347f1755b57309535cfa68e6ac41829
kenneth_jcjw <kenneth_jcjw@aiap.sg>
2022-12-23 18:32:58 +0800

    working

--- paperparser.py
+++ paperparser.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 class PaperParser():
-    def __init__(self, corpus_text=None, model="small") -> None:
+    def __init__(self,corpus_text=None,model="small") -> None:
         """
         Model choices are:-
         1. large - length of embeddings per token is 4096
@@ -16,20 +16,21 @@ class PaperParser():
         3. multilingual-22-12
         """
         self.model = model
-        self.models = ["small", "large", "multilingual-22-12"]
+        self.models = ["small","large","multilingual-22-12"]
         self.client = cohere.Client("7lUDtMMSa1bVCEVIKEOms0jPImRnselfUQucOH5v")
         self.corpus = None
         # self.corpus_embeddings = co.embed(texts=corpus_text,model=model).embeddings
+    
 
-    def create_corpus(self, datafile, file_type="csv"):
+    def create_corpus(self,datafile,file_type="csv"):
         if type != "csv":
-            data = pd.read_csv(datafile, sep="\t")
+            data = pd.read_csv(datafile,sep="\t")
         else:
             data = pd.read_csv(datafile)
         corpus = {}
         corpus_records = data.to_dict(orient="records")
 
-        for idx, record in enumerate(corpus_records):
+        for idx,record in enumerate(corpus_records):
             corpus[idx] = record
         self.corpus = corpus
 
@@ -48,8 +49,8 @@ class PaperParser():
         corpus_embeddings = self.client.embed(texts=corpus_texts,model=self.model).embeddings
         for idx,embedding in enumerate(corpus_embeddings):
             self.corpus[idx]["EMBEDDING"] = embedding
-
-    def calc_tf_idf(self, text, result="df", top_n=10):
+        
+    def calc_tf_idf(self,text,result="df",top_n=10):
         """
         Calculates the TF-IDF of the ABSTRACT field in the corpus and returns either
         the whole matrix or the top N results
@@ -80,14 +81,14 @@ class PaperParser():
         
         return_result = []
         if result == "top":
-            for row, index in df.iterrows():
+            for row,index in df.iterrows():
                 # return_result.append(index)
-                return_result.append(index.sort_values(ascending=False)[:top_n])
+                return_result.append(index.sort_values(ascending=False)[:top_n].to_string().replace(" ","-*-").replace("\n",", ").replace("-*-"," "))
             return return_result
         else:
             return df
     
-    def get_cos_sim(self, text: str, top_n=5) -> List[Tuple[str, float]]:
+    def get_cos_sim(self,text: str,top_n=5) -> List[Tuple[str, float]]:
         """Return cosine similarity scores (sorted in descending order) of corpus documents with input text.
 
         Args:
@@ -98,19 +99,30 @@ class PaperParser():
         """
         # Get embedding
         input_text = [text]
-        text_embeddings = self.client.embed(texts=input_text, model=self.model).embeddings
+        text_embeddings = self.client.embed(texts=input_text,model=self.model).embeddings
         # Get cosine similarities
         res = []
         tf_idf_texts = []
         for record in self.corpus.items():
             cos_sim = dot(text_embeddings, record[1]["EMBEDDING"])/(norm(text_embeddings)*norm(record[1]["EMBEDDING"]))
-            tf_idf_results = self.calc_tf_idf(text=record[1]["ABSTRACT"], result="top", top_n=top_n)
+            tf_idf_results = self.calc_tf_idf(text=record[1]["ABSTRACT"],result="top",top_n=top_n)
             print(tf_idf_results)
-            tf_idf_texts.append((record[1]["TITLE"],record[1]["ABSTRACT"], float(cos_sim)))
-            res.append((record[1]["TITLE"],float(cos_sim), tf_idf_results))
+            tf_idf_texts.append((record[1]["TITLE"],record[1]["ABSTRACT"],float(cos_sim)))
+            res.append((record[1]["TITLE"],float(cos_sim),tf_idf_results))
+        # tf_idf_results = self.calc_tf_idf(text=tf_idf_texts,result="top",top_n=top_n)
         res.sort(key=lambda a:a[1],reverse=True)
         tf_idf_texts.sort(key=lambda a:a[2],reverse=True)
+        # print(res)
+        # print(tf_idf_texts)
+        # tf_idf_results = self.calc_tf_idf(text=tf_idf_texts,result="top",top_n=top_n)
+        # print(tf_idf_results)
         if len(res) >= top_n:
             return res[:top_n]
+            # return res[:top_n],tf_idf_results
         else:
             return res
+            # return res,tf_idf_results
+
+        # return res
+    
+    
